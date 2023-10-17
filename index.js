6fa902743b6263c5caa46eca289ecfdb8c2a86eb
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2023-10-17 22:29:15 +0000

    update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const fs = require('fs')
 const path = require('path')
 
 const redirectsFile = fs.readFileSync(path.join(__dirname,'redirects.yml'), 'utf-8')
+const redirects = YAML.parse(redirectsFile)
+
+console.log(redirectsFile)
+console.log(redirects)
 
 // Generating HTML for each url from template.html
-
+const templateHTML = fs.readFileSync(path.join(__dirname,'template.html '), 'utf-8')
