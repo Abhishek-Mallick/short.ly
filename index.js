af3cedc0aa01b9c49cf8354ff1652e363f5c096e
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2023-10-17 22:35:16 +0000

    loop through redirects

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const path = require('path')
 const redirectsFile = fs.readFileSync(path.join(__dirname,'redirects.yml'), 'utf-8')
 const redirects = YAML.parse(redirectsFile)
 
-console.log(redirectsFile)
+// console.log(redirectsFile)
 console.log(redirects)
 
 // Generating HTML for each url from template.html
-const templateHTML = fs.readFileSync(path.join(__dirname,'template.html '), 'utf-8')
+const templateHTML = fs.readFileSync(path.join(__dirname,'template.html'), 'utf-8')
+
+// console.log(Object.entries(redirects))
+// Loop through all url redirects, and generate HTML pages
+for(let [slug,url] of Object.entries(redirects)) {
+    console.log('Generateing HTML Page for ',slug)
+}
